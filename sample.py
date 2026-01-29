d2f3efe1229593ef6ca12ee202310afbee8cc7f0
xinxiang <221850120@smail.nju.edu.cn>
2026-01-29 20:34:39 +0800

    new:更新sample结构

--- sample.py
+++ sample.py
@@ -1,29 +1,30 @@
-def generate_sample():
-    base_sample = {
-        "target": {"l": 0, "r": 1, "rel": "P"},
-        "events": ["Meeting", "Conference", "Workshop"],
-        "formulas": [
-            {"l": 0, "r": 2, "rel": "f", "hints": [0, 1]},
-            {"l": 2, "r": 1, "rel": "O", "hints": [2]},
-            {"l": 0, "r": 1, "rel": "O", "type": "not", "hints": [4]},
-            {"l": 0, "r": 1, "rel": "M", "type": "not", "hints": [4]},
-        ],
-        "hints": [
-            "event 0 takes place from 2022.9.12 to 2022.12.5.",
-            "event 2 occurs from 2022.8.20 to 2022.12.5.",
-            "event 2 was overlapped by event 1.",
-            "There is no overlap between event 0 and event 1.",
-        ],
-    }
-
-
-# how to recursively generate more complex samples?
-def generate_complex_sample(base_sample, formula_num: int):
-    # based on the choosed formulas, generate more complex samples
-    target = {
-        "l": base_sample["formulas"][formula_num]["l"],
-        "r": base_sample["formulas"][formula_num]["r"],
-        "rel": base_sample["formulas"][formula_num]["rel"],
-    }
-
-    
+base_sample = {
+    "target": {"l": 0, "r": 1, "rel": "P"},
+    "events": ["Meeting", "Conference", "Workshop"],
+    "hints": [
+        "event 0 takes place from 2022.9.12 to 2022.12.5.",
+        "event 2 occurs from 2022.8.20 to 2022.12.5.",
+        "event 2 was overlapped by event 1.",
+        "There is no overlap between event 0 and event 1.",
+    ],
+    "paths": [
+        {
+            "target": "P",  # target relation
+            "path": ["f", "O"],  # composition path
+            "excluded": ["O", "M"],  # relations to be excluded
+            "parent": -1,  # parent path index, -1 if root
+            "left": 1,  # paths[1] is left child
+            "right": -1,  # no right if -1
+            "base_event": [0, 1],  # base events for target relation
+            "new_event": 2,  # new event introduced in this path
+        },
+        {
+            "target": "f",
+            "path": "fe",
+            "parent": 0,
+            "base_event": [0, 2],
+            "new_event": 3,
+            "excluded": "",
+        },
+    ],
+}
