ff8cca464dd3c74f7c6c1c32fab850e6c996d3e2
xinxiang <221850120@smail.nju.edu.cn>
2026-01-31 15:02:45 +0800

    perf:简单修改了一下验证结果的显示

--- verifier.py
+++ verifier.py
@@ -13,23 +13,31 @@ def main():
     level2 = 0
     level1_true = 0
     level2_true = 0
-    i = 0
+    i, j = 0, 0
     for item in data:
         check = 1 if item["answer_single"][0].strip() == item["target"]["rel"] else 0
         if item["level"] == 1:
             level1 += 1
             level1_true += check
-        elif item["level"] == 2:
-            level2 += 1
-            level2_true += check
-            if check == 0:
+            if check == 0 and i < 20:
                 answer = item["answer_single"][0].strip()
                 if answer.startswith("<think>"):
                     answer = "None"
-                print(f"错误样例: target={item['target']['rel']}, answer={answer}")
+                print(
+                    f"level1错误样例: target={item['target']['rel']}, answer={answer}"
+                )
                 i += 1
-        # if i > 10:
-        # break
+        elif item["level"] == 2:
+            level2 += 1
+            level2_true += check
+            if check == 0 and j < 20:
+                answer = item["answer_single"][0].strip()
+                if answer.startswith("<think>"):
+                    answer = "None"
+                print(
+                    f"level2错误样例: target={item['target']['rel']}, answer={answer}"
+                )
+                j += 1
 
     print(f"Level 1 Accuracy: {level1_true}/{level1} = {level1_true/level1:.4f}")
     print(f"Level 2 Accuracy: {level2_true}/{level2} = {level2_true/level2:.4f}")
